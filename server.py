4939e205a46cc8bf92c4d49393d815bbc73120f5
Yuxuan Luo <root1@0587377785.wireless.umich.net>
2020-01-29 10:27:10 -0500

    file read ready

--- server.py
+++ server.py
@@ -1,17 +1,29 @@
-from ordered_set import OrderedSet
+
 
 def main():
-    CurrentVersion = -1
-    CurrentMenu = OrderedSet() 
+  CurrentVersion = -1
+  CurrentMenu = set()
 
-    MenuFile = open('/Users/root1/Desktop/list.txt', 'r')
-    Lines = MenuFile.readlines() 
-    CurrentVersion = Lines[0]
-    for i in range(1,len(Lines)):
-        print(Lines[i][:-1])
+  VersionFile = open("LatestVersion.txt", 'r')
+  Lines = VersionFile.readlines() 
+  CurrentVersion = Lines[0]
+  VersionFile.close()
+  print("Current Version is: " + CurrentVersion)
+
+  MenuFile = open("Menu.txt", 'r')
+  Lines = MenuFile.readlines() 
+
+  for item in Lines:
+    if item != "" and item != "\n":
+      if item[-1] == "\n":
+        CurrentMenu.add(item[:-1])
+      else:
+        CurrentMenu.add(item)
+
+  
+  print("Current Menu:")
+  print(CurrentMenu)
 
         
-
-
 if __name__== "__main__":
   main()
