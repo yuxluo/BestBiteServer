05c72405c9f2f35a57795e4bc9232442b89730ab
Yuxuan Luo <root1@0587371616.wireless.umich.net>
2020-01-29 13:59:47 -0500

    working for one time

--- server.py
+++ server.py
@@ -1,12 +1,16 @@
 from html.parser import HTMLParser
 import os
 import copy 
+import configparser
 
 InComingMenuItem = False 
 ExistingMenu = set()
-
+UpdatedMenu = set()
+StringsLib = configparser.RawConfigParser()
+CurrentVersion = -1
 
 def parse_menu():
+  global StringsLib
   class MyHTMLParser(HTMLParser):
 
     def handle_starttag(self, tag, attrs):
@@ -16,24 +20,38 @@ def parse_menu():
 
     def handle_data(self, data):
       global InComingMenuItem
+      global UpdatedMenu
+
       if InComingMenuItem == True:
-        print("Menu Item: ", data)
-        InComingMenuItem = False
+        sanitized = data.strip()
+        if sanitized not in UpdatedMenu:
+          UpdatedMenu.add(sanitized)
+      InComingMenuItem = False
 
   parser = MyHTMLParser()
-  os.system("wget https://dining.umich.edu/menus-locations/dining-halls/bursley/")
+  
+  for key in StringsLib['URL']:
+    process_dining_hall(key, parser)
+
+def process_dining_hall(dining_hall_name, parser):
+  global StringsLib
+  print("@@@ Processing Today's Menu for " + dining_hall_name)
+  os.system("wget " + StringsLib.get('URL', dining_hall_name) + ">/dev/null 2>&1")
   with open('index.html', 'r') as file:
     data = file.read().replace('\n', '')
   parser.feed(data)
   os.system("rm index.html")
 
 
-def RecoverData(CurrentVersion, CurrentMenu):
+def RecoverData():
+  global ExistingMenu
+  global CurrentVersion
+
   VersionFile = open("LatestVersion.txt", 'r')
   Lines = VersionFile.readlines() 
-  CurrentVersion = Lines[0]
+  CurrentVersion = int(Lines[0])
   VersionFile.close()
-  print("Current Version is: " + CurrentVersion)
+  print("Current Version is: " + str(CurrentVersion))
 
   MenuFile = open("Menu.txt", 'r')
   Lines = MenuFile.readlines() 
@@ -41,22 +59,51 @@ def RecoverData(CurrentVersion, CurrentMenu):
   for item in Lines:
     if item != "" and item != "\n":
       if item[-1] == "\n":
-        CurrentMenu.add(item[:-1])
+        ExistingMenu.add(item[:-1])
       else:
-        CurrentMenu.add(item)
+        ExistingMenu.add(item)
+
+  MenuFile.close()
+  print("Current Menu:")
+  print(ExistingMenu)
 
 
 def main():
-  CurrentVersion = -1
-  CurrentMenu = set()
-
-  RecoverData(CurrentVersion, CurrentMenu)
-  print("Current Menu:")
-  print(CurrentMenu)
-
+  global StringsLib
   global ExistingMenu
-  ExistingMenu = copy.deepcopy(CurrentMenu)
+  global UpdatedMenu
+  global CurrentVersion
+
+  StringsLib.read('strings.properties')
+  RecoverData()
+  UpdatedMenu = copy.deepcopy(ExistingMenu)
+
   parse_menu()
+  if len(UpdatedMenu.difference(ExistingMenu)) != 0:
+    UpdateMenu()
+  else:
+    print("@@@ No New Update")
+  
+
+def UpdateMenu():
+  global ExistingMenu
+  global UpdatedMenu
+  global CurrentVersion
+  CurrentVersion += 1
+
+  print("@@@ " + str(len(UpdatedMenu.difference(ExistingMenu))) + " new updates to menu")
+  print("@@@ Writing Updates to Version " + str(CurrentVersion) + " of the menu")
+
+  VersionFile = open("LatestVersion.txt", 'w')
+  VersionFile.writelines(str(CurrentVersion))
+  VersionFile.close()
+
+  MenuFile = open("Menu.txt", 'w')
+  for dish in sorted(UpdatedMenu):
+    MenuFile.writelines(dish + "\n")
+  MenuFile.close()
+
+  ExistingMenu = copy.deepcopy(UpdatedMenu)
 
 if __name__== "__main__":
   main()
