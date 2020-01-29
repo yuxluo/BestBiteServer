69be00c2284c03bdb41370b3597e0b4824f95dd1
Yuxuan Luo <root1@0587371616.wireless.umich.net>
2020-01-29 12:59:35 -0500

    parser

--- server.py
+++ server.py
@@ -1,9 +1,34 @@
+from html.parser import HTMLParser
+import os
+import copy 
+
+InComingMenuItem = False 
+ExistingMenu = set()
 
 
-def main():
-  CurrentVersion = -1
-  CurrentMenu = set()
+def parse_menu():
+  class MyHTMLParser(HTMLParser):
 
+    def handle_starttag(self, tag, attrs):
+      global InComingMenuItem
+      if tag == "div" and attrs[0] == ('class', 'item-name'):
+        InComingMenuItem = True
+
+    def handle_data(self, data):
+      global InComingMenuItem
+      if InComingMenuItem == True:
+        print("Menu Item: ", data)
+        InComingMenuItem = False
+
+  parser = MyHTMLParser()
+  os.system("wget https://dining.umich.edu/menus-locations/dining-halls/bursley/")
+  with open('index.html', 'r') as file:
+    data = file.read().replace('\n', '')
+  parser.feed(data)
+  os.system("rm index.html")
+
+
+def RecoverData(CurrentVersion, CurrentMenu):
   VersionFile = open("LatestVersion.txt", 'r')
   Lines = VersionFile.readlines() 
   CurrentVersion = Lines[0]
@@ -20,10 +45,18 @@ def main():
       else:
         CurrentMenu.add(item)
 
-  
+
+def main():
+  CurrentVersion = -1
+  CurrentMenu = set()
+
+  RecoverData(CurrentVersion, CurrentMenu)
   print("Current Menu:")
   print(CurrentMenu)
 
-        
+  global ExistingMenu
+  ExistingMenu = copy.deepcopy(CurrentMenu)
+  parse_menu()
+
 if __name__== "__main__":
   main()
